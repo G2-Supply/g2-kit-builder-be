68854ce93a02437d1a7bef4e3552368e2e3fbe2b
Elijah McKay <elijahmckay10@gmail.com>
2020-01-13 18:59:36 -0500

    got initial email functionality working for quoting

--- routes/quote-routes.js
+++ routes/quote-routes.js
@@ -2,6 +2,7 @@
 // a 1 liner short cut for creating a router with express
 const router = require('express').Router(); 
 require('dotenv').config(); 
+const nodemailer = require('nodemailer'); 
 
 // middleware imports;
 
@@ -28,46 +29,109 @@ router.get('/:_id', (req, res) => {
         }); 
 });
 
-router.post('/:_id', (req, res) => {
-
-    const kitId = req.body.kitId; 
-    console.log(req.body);  
-    Pallets.findById(kitId) 
-    .then(docs => {
-        res.status(200).json({ docs }); 
-    })
-    .catch(err => { 
-        res.status(500).json({ error: err }); 
-    }); 
-})
 
 // ======================== POST Requests ===========================
 // create new kit for a specific user
-router.post('/:_id', (req, res) => {
-    console.log(req.body.kitId); 
+// router.post('/:_id', (req, res) => {
+//     console.log(req.body.kitId); 
 
-    const kit = new Kits({
-        user_id: req.params,
-    });
+//     const kit = new Kits({
+//         user_id: req.params,
+//     });
   
-    // saving the user to the users collection
-    kit.save()
-    .then(kit => {
-        res.status(201).json({ kit })
-    })
-    .catch(err => {
-        res.status(500).json({ error: `${err}` })
-    });
-});
+//     // saving the user to the users collection
+//     kit.save()
+//     .then(kit => {
+//         res.status(201).json({ kit })
+//     })
+//     .catch(err => {
+//         res.status(500).json({ error: `${err}` })
+//     });
+// });
 
 
 // send a kit to G2 for quoting
 router.post('/:_id', (req, res) => {
-    const kitId = req.body.kitId; 
+    const kit_id = req.body.kitId; 
 
-    Kits.findById(kitId) 
+    const kitArr = []
+
+    Pallets.find({ kit_id })
     .then(docs => {
-        res.status(200).json({ docs }); 
+        kitArr.push(docs[0]); 
+        // res.status(201).json({ kitArr })
+    })
+    .catch(err => { 
+        res.status(500).json({ error: err }); 
+    }); 
+
+    Boxes.find({ kit_id })
+    .then(docs => {
+        kitArr.push(docs[0]); 
+        // res.status(201).json({ kitArr })
+    })
+    .catch(err => { 
+        res.status(500).json({ error: err }); 
+    }); 
+
+    BoxLids.find({ kit_id })
+    .then(docs => {
+        kitArr.push(docs[0]); 
+        // res.status(201).json({ kitArr })
+    })
+    .catch(err => { 
+        res.status(500).json({ error: err }); 
+    }); 
+
+    Dividers.find({ kit_id })
+    .then(docs => {
+        kitArr.push(docs[0]); 
+        // res.status(201).json({ kitArr }) 
+    })
+    .catch(err => { 
+        res.status(500).json({ error: err }); 
+    }); 
+
+    Foam.find({ kit_id })
+    .then(docs => {
+        kitArr.push(docs[0]); 
+        // res.status(201).json({ kitArr })
+    })
+    .catch(err => { 
+        res.status(500).json({ error: err }); 
+    }); 
+
+    OrderDetails.find({ kit_id })
+    .then(docs => {
+        kitArr.push(docs[0]); 
+        // res.status(201).json({ kitArr }) 
+        res.status(201).json({ kitArr })
+
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: `${process.env.EMAIL_ADDRESS}`,
+                pass: `${process.env.EMAIL_PASSWORD}`
+            }
+        }); 
+
+        const mailOptions = {
+            from: `${process.env.EMAIL_ADDRESS}`,
+            to: `elijahmckay10@gmail.com`,
+            subject: 'G2 Kit Builder - Quote',
+            text: `
+              You are receiving a quote ${kitArr}
+            `
+        }
+
+        transporter.sendMail(mailOptions, function(err, response) {
+            if (err) {
+                console.error('There was an issue: ', err); 
+            } else {
+                console.log('Response: ', response); 
+            }
+        }); 
+
     })
     .catch(err => { 
         res.status(500).json({ error: err }); 
