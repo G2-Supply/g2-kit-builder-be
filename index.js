fecf7e09b009d96de68609d3f77792cd435c785d
Elijah McKay <elijahmckay10@gmail.com>
2020-01-20 23:26:18 -0600

    fixed PORT variable in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const server = require('./server.js'); 
 
 // uses port 5000 for development, .env for production
-const PORT = 5000 || process.env.PORT; 
+const PORT = process.env.PORT || 5000; 
 
 // display a log for our live server
 server.listen(PORT, () => console.log(`Listening on port ${PORT}...`)); 
